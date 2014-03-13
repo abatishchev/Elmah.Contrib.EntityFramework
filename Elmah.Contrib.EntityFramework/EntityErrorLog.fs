1e17b81a4c8281a87ac4b4bc22cb541c39d96c40
Alexander Batishchev <abatishchev@gmail.com>
2014-03-12 21:11:11 -0700

    Implementing using System.Configuration

--- Elmah.Contrib.EntityFramework/EntityErrorLog.fs
+++ Elmah.Contrib.EntityFramework/EntityErrorLog.fs
@@ -1,9 +1,26 @@
 ﻿namespace Elmah.Contrib.EntityFramework
 
+open System.Configuration
+open System.Data.Entity.Core.EntityClient
+
 open Elmah
 
-type public EntityErrorLog(config : System.Collections.IDictionary) = 
-    inherit SqlErrorLog(config)
+type public EntityErrorLog = 
+    inherit SqlErrorLog
+
+    val private _connectionStringName : string
+
+    new (config : System.Collections.IDictionary) = {
+            inherit SqlErrorLog(config)
+
+            // config is a non-generic dictionary thus casting to string is required
+            _connectionStringName = config.Item("connectionStringName") :?> string
+        }
 
     override this.ConnectionString
-        with get() = "F#"
+        with get() = this.GetProviderPart(this._connectionStringName)
+
+    member private this.GetProviderPart(connectionStringName : string) =
+        let entityConnectionString = ConfigurationManager.ConnectionStrings.Item(connectionStringName).ConnectionString;
+        let builder = new EntityConnectionStringBuilder(entityConnectionString)
+        builder.ProviderConnectionString
