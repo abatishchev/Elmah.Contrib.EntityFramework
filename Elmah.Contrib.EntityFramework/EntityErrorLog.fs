9b6133a5a01eaf755eca9e13c2b98dc308187c71
Alexander Batishchev <abatishchev@gmail.com>
2014-03-12 20:57:11 -0700

    Adding EntityErrorLog with stub implementation

---
+++ Elmah.Contrib.EntityFramework/EntityErrorLog.fs
@@ -0,0 +1,9 @@
+﻿namespace Elmah.Contrib.EntityFramework
+
+open Elmah
+
+type public EntityErrorLog(config : System.Collections.IDictionary) = 
+    inherit SqlErrorLog(config)
+
+    override this.ConnectionString
+        with get() = "F#"
