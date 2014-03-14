560f5435934c022b057a81a8df85985664cac688
Alexander Batishchev <abatishchev@gmail.com>
2014-03-13 17:42:21 -0700

    Including password into the unit test. Updating nuspec. Replacing build.cmd with build.ps1

--- Elmah.Contrib.EntityFramework.Test/EntityErrorLogTest.fs
+++ Elmah.Contrib.EntityFramework.Test/EntityErrorLogTest.fs
@@ -7,10 +7,10 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 [<TestClass>]
 type JsonBlobNameResolverTest() =
     [<TestMethod>]
-    member this.ConnectionString_Should_Return_Provider_Part_From_Connection_String() =
+    member this.ConnectionString_Should_Return_Provider_Part() =
 
         // Arrange
-        let providerConectionString = "Data Source=localhost;Initial Catalog=Northwind"
+        let providerConectionString = "Data Source=localhost;Initial Catalog=Northwind;Password=Alpine"
         let entityConectionString = sprintf "metadata=res://*/;provider=System.Data.SqlClient;provider connection string=\"%s\";" providerConectionString
 
         let config = new System.Collections.Generic.Dictionary<string,string>()
