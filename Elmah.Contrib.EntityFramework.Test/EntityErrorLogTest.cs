2d93247d3003169a42fc58bb65f39c941143755e
Alexander Batishchev <abatishchev@gmail.com>
2014-08-14 13:54:40 -0700

    Replacing the F# test project with C#

---
+++ Elmah.Contrib.EntityFramework.Test/EntityErrorLogTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Elmah.Contrib.EntityFramework.Test
+{
+    [TestClass]
+    public class EntityErrorLogTest
+    {
+        [TestMethod]
+        public void ConnectionString_Should_Return_Provider_Part()
+        {
+            // Arrange
+            const string providerConectionString = "Data Source=localhost;Initial Catalog=Northwind;Password=Alpine";
+            string entityConectionString = String.Format("metadata=res://*/;provider=System.Data.SqlClient;provider connection string=\"{0}\";", providerConectionString);
+
+            var config = new System.Collections.Generic.Dictionary<string, string>();
+            config["connectionString"] = entityConectionString;
+
+            // Act
+            var entityErrorLog = new EntityErrorLog(config);
+
+            // Assert
+            entityErrorLog.ConnectionString.Should().Be(providerConectionString);
+        }
+    }
+}
---
+++ Elmah.Contrib.EntityFramework.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Elmah.Contrib.EntityFramework.Test")]
+[assembly: AssemblyProduct("Elmah.Contrib.EntityFramework.Test")]
+
+[assembly: AssemblyCopyright("Copyright © Alexander Batishchev 2014")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: Guid("0240378a-44bf-4150-b951-7fbb292b54f7")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
